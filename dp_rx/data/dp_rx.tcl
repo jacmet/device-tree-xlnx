jacmet/device-tree-xlnx

commit edb1f978f719b18c8736e31276e38a5fb331aa29

vid_phy_ctrl:hdmi_gt_ctrl: Generate Channels only if connected to DP/HDMI
vid_phy_controller is generating the channel nodes based on the
tx/rx channels count. Check if channel pin is connected or not
and check memory mapped or not then generate the channel nodes.

Add pinname + connected IP instances as node name and
vid IP instance + txphy_lane0/1/2/3 as label.
This will help when multiple phy_controllers are present
in design it will generate the nodes with unique names.

Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
Acked-by: Varalaxmi Bingi <Varalaxmi.bingi@amd.com>

dp_rx/data/dp_rx.tcl
@@ -64,9 +64,13 @@ proc generate {drv_handle} {
 	hsi::utils::add_new_dts_param "${node}" "xlnx,dp-retimer" "xfmc" reference
 	set i 0
 	while {$i < $lane_count} {
-		set phy_s "rxphy_lane$i 0 1 1 0"
-		set clocks [lappend clocks $phy_s]
-		set updat  [lappend updat $phy_s]
+		set txpinname "m_axis_lnk_rx_lane$i"
+		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $txpinname]
+		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
+			set phy_s "${channelip}rxphy_lane${i} 0 1 1 1"
+			set clocks [lappend clocks $phy_s]
+			set updat  [lappend updat $phy_s]
+		}
 		incr i
 	}
 	set len [llength $updat]

dp_tx/data/dp_tx.tcl
@@ -54,9 +54,13 @@ proc generate {drv_handle} {
 	}
 	set i 0
 	while {$i < $lane_count} {
-		set phy_s "txphy_lane$i 0 1 1 1"
-		set clocks [lappend clocks $phy_s]
-		set updat  [lappend updat $phy_s]
+		set txpinname "m_axis_lnk_tx_lane$i"
+		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $txpinname]
+		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
+			set phy_s "${channelip}txphy_lane${i} 0 1 1 1"
+			set clocks [lappend clocks $phy_s]
+			set updat  [lappend updat $phy_s]
+		}
 		incr i
 	}
 	set len [llength $updat]

hdmi_gt_ctrl/data/hdmi_gt_ctrl.tcl
@@ -62,8 +62,12 @@ proc generate {drv_handle} {
 	set hdmi_fast_switch [get_property CONFIG.C_Hdmi_Fast_Switch [get_cells -hier $drv_handle]]
 	hsi::utils::add_new_dts_param "${node}" "xlnx,hdmi-fast-switch" $hdmi_fast_switch int
 	for {set ch 0} {$ch < $tx_no_of_channels} {incr ch} {
-		set phy_node [add_or_get_dt_node -n "txphy_lane@$ch" -l txphy_lane$ch -p $node]
-		hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		set txpinname "tx_axi4s_ch$ch"
+		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $txpinname]
+		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
+			set phy_node [add_or_get_dt_node -n "${txpinname}${channelip}" -l ${drv_handle}txphy_lane${ch} -p $node]
+			hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		}
 	}
 	set transceiver [get_property CONFIG.Transceiver [get_cells -hier $drv_handle]]
 	switch $transceiver {

hdmi_rx_ss/data/hdmi_rx_ss.tcl
@@ -72,10 +72,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data0]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data0]
 		if {[llength $ip_mem_handles]} {
+			set link_data0_inst $link_data0
 			set link_data0 [get_property IP_NAME $link_data0]
 			if {[string match -nocase $link_data0 "vid_phy_controller"] || [string match -nocase $link_data0 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy0"
-				append phys  "rxphy_lane0 0 1 1 0>,"
+				append phys  "${link_data0_inst}rxphy_lane0 0 1 1 0>,"
 			}
 		} else {
 			dtg_warning "connected stream of LINK_DATA0_IN is NULL...check the design"
@@ -85,10 +86,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data1]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data1]
 		if {[llength $ip_mem_handles]} {
+			set link_data1_inst $link_data1
 			set link_data1 [get_property IP_NAME $link_data1]
 			if {[string match -nocase $link_data1 "vid_phy_controller"] || [string match -nocase $link_data1 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy1"
-				append phys  " <&rxphy_lane1 0 1 1 0>,"
+				append phys  " <&${link_data1_inst}rxphy_lane1 0 1 1 0>,"
 			}
 		} else {
 			dtg_warning "Connected stream of LINK_DATA1_IN is NULL...check the design"
@@ -98,10 +100,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data2]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data2]
 		if {[llength $ip_mem_handles]} {
+			set link_data2_inst $link_data2
 			set link_data2 [get_property IP_NAME $link_data2]
 			if {[string match -nocase $link_data2 "vid_phy_controller"] || [string match -nocase $link_data2 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy2"
-				append phys " <&rxphy_lane2 0 1 1 0"
+				append phys " <&${link_data2_inst}rxphy_lane2 0 1 1 0"
 			}
 		} else {
 			dtg_warning "Connected stream of LINK_DATA2_IN is NULL...check the design"
@@ -111,10 +114,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data3]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data3]
 		if {[llength $ip_mem_handles]} {
+			set link_data3_inst $link_data3
 			set link_data3 [get_property IP_NAME $link_data3]
 			if {[string match -nocase $link_data3 "vid_phy_controller"] || [string match -nocase $link_data3 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy3"
-				append phys " <&rxphy_lane3 0 1 1 0"
+				append phys " <&${link_data3_inst}rxphy_lane3 0 1 1 0"
 			}
 		} else {
 			dtg_warning "Connected stream of LINK_DATA3_IN is NULL...check the design"

hdmi_tx_ss/data/hdmi_tx_ss.tcl
@@ -41,10 +41,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data0]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data0]
 		if {[llength $ip_mem_handles]} {
+			set link_data0_inst $link_data0
 			set link_data0 [get_property IP_NAME $link_data0]
 			if {[string match -nocase $link_data0 "vid_phy_controller"] || [string match -nocase $link_data0 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy0"
-				append phys  "txphy_lane0 0 1 1 1>,"
+				append phys  "${link_data0_inst}txphy_lane0 0 1 1 1>,"
 			}
 		}
 	} else {
@@ -55,10 +56,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data1]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data1]
 		if {[llength $ip_mem_handles]} {
+			set link_data1_inst $link_data1
 			set link_data1 [get_property IP_NAME $link_data1]
 			if {[string match -nocase $link_data1 "vid_phy_controller"] || [string match -nocase $link_data1 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy1"
-				append phys  " <&txphy_lane1 0 1 1 1>,"
+				append phys  " <&${link_data1_inst}txphy_lane1 0 1 1 1>,"
 			}
 		}
 	} else {
@@ -69,10 +71,11 @@ proc generate {drv_handle} {
 	if {[llength $link_data2]} {
 		set ip_mem_handles [hsi::utils::get_ip_mem_ranges $link_data2]
 		if {[llength $ip_mem_handles]} {
+			set link_data2_inst $link_data2
 			set link_data2 [get_property IP_NAME $link_data2]
 			if {[string match -nocase $link_data2 "vid_phy_controller"] || [string match -nocase $link_data2 "hdmi_gt_controller"]} {
 				append phy_names " " "hdmi-phy2"
-				append phys " <&txphy_lane2 0 1 1 1"
+				append phys " <&${link_data2_inst}txphy_lane2 0 1 1 1"
 			}
 		}
 	} else {

vid_phy_ctrl/data/vid_phy_ctrl.tcl
@@ -61,12 +61,20 @@ proc generate {drv_handle} {
 		hsi::utils::add_new_dts_param "${node}" "xlnx,use-gt-ch4-hdmi" $use_gt_ch4_hdmi int
 	}
 	for {set ch 0} {$ch < $Rx_No_Of_Channels} {incr ch} {
-		set phy_node [add_or_get_dt_node -n "rxphy_lane@$ch" -l rxphy_lane$ch -p $node]
-		hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		set rxpinname "vid_phy_rx_axi4s_ch$ch"
+		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $rxpinname]
+		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
+			set phy_node [add_or_get_dt_node -n "${rxpinname}${channelip}" -l ${drv_handle}rxphy_lane${ch} -p $node]
+			hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		}
 	}
 	for {set ch 0} {$ch < $tx_no_of_channels} {incr ch} {
-		set phy_node [add_or_get_dt_node -n "txphy_lane@$ch" -l txphy_lane$ch -p $node]
-		hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		set txpinname "vid_phy_tx_axi4s_ch$ch"
+		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $txpinname]
+		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
+			set phy_node [add_or_get_dt_node -n "${txpinname}${channelip}" -l ${drv_handle}txphy_lane${ch} -p $node]
+			hsi::utils::add_new_dts_param "$phy_node" "#phy-cells" 4 int
+		}
 	}
 	set transceiver [get_property CONFIG.Transceiver [get_cells -hier $drv_handle]]
 	switch $transceiver {

commit 5c18f27c8a9128343a5c4aeb59e6e28f013489a0

dp_rx/data/dp_rx.tcl: unbreak vid_phy direction
Commit edb1f978f719b (vid_phy_ctrl:hdmi_gt_ctrl: Generate Channels only if
connected to DP/HDMI) broke the vid_phy direction setting as it set the 4th cell to 1 instead of 0.

As specified by the binding, the 4th cell encodes the phy directions:

  direction_tx = 0 for RX and 1 for TX

https://github.com/Xilinx/dp-modules/blob/xlnx_rel_v2024.1/Documentation/devicetree/bindings/xlnx%2Cvphy.txt

So setting it to 1 doesn't make any sense for the DP receiver core.  This
seems to be a cut'n'paste mistake from the similar logic in the DP
transmitter core.

Fixes: edb1f978f719b ("vid_phy_ctrl:hdmi_gt_ctrl: Generate Channels only if
       connected to DP/HDMI")
Signed-off-by: Peter Korsgaard <peter.korsgaard@barco.com>

dp_rx/data/dp_rx.tcl
@@ -72,7 +72,7 @@ proc generate {drv_handle} {
 		set rxpinname "s_axis_lnk_rx_lane$i"
 		set channelip [get_connected_stream_ip [get_cells -hier $drv_handle] $rxpinname]
 		if {[llength $channelip] && [llength [hsi::utils::get_ip_mem_ranges $channelip]]} {
-			set phy_s "${channelip}rxphy_lane${i} 0 1 1 1"
+			set phy_s "${channelip}rxphy_lane${i} 0 1 1 0"
 			set clocks [lappend clocks $phy_s]
 			set updat  [lappend updat $phy_s]
 		}